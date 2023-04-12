e38ddadd8a936d27ebbec798d851e4695c364d89
SANGJAEY\SANGJAEY <sangjaey@hanmail.net>
2023-04-12 22:22:13 +0900

    단위변환 코딩_뷰바인딩

--- KOTLIN/MyUnitTrans/app/src/main/java/alsatang/co/kr/myunittrans/MainActivity.kt
+++ KOTLIN/MyUnitTrans/app/src/main/java/alsatang/co/kr/myunittrans/MainActivity.kt
@@ -1,11 +1,65 @@
 package alsatang.co.kr.myunittrans
 
+import alsatang.co.kr.myunittrans.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        var view = binding.root
+        setContentView(view)
+
+        val edtInput = binding.edtInput
+        val txtOutput = binding.txtOutput
+        val txtInputUnit = binding.txtInputUnit
+        val txtOutputUnit = binding.txtOutputUnit
+        val btnSwitch = binding.btnSwitch
+
+        var  inputNumber:Int = 0
+        var cmToM = true
+
+        edtInput.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrEmpty()){
+                0
+            }
+            else
+            {
+                text.toString().toInt()
+            }
+            Log.d("INPUTNUMBER", inputNumber.toString())
+
+            var outNumber =
+            if(cmToM){
+                inputNumber.times(0.01)
+            }else
+            {
+                inputNumber.times(100)
+            }
+            txtOutput.text = outNumber.toString()
+        }
+
+        btnSwitch.setOnClickListener {
+            cmToM = cmToM.not()
+            if(cmToM) {
+                txtInputUnit.text = "cm"
+                txtOutputUnit.text = "m"
+                txtOutput.text = inputNumber.times(0.01).toString()
+            }
+            else
+            {
+                txtInputUnit.text = "m"
+                txtOutputUnit.text = "Cm"
+                txtOutput.text = inputNumber.times(100).toString()
+            }
+        }
+
     }
 }
